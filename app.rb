0e74857c0da914dd6715aabd6908ae203b5f5507
Matt <matt.fasano88@gmail>
2017-04-13 17:52:55 -0400

    SolarUpdate

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require "sinatra"
+require 'sendgrid-ruby'
+include SendGrid
 
 # Home Page
 get '/' do
@@ -14,11 +16,30 @@ end
 
 # Photos page
 get "/photos" do
-  "Contact info"
+  "Photos Gallery"
     erb :photos
 end
 # Contact Page
 get "/contact" do
   "Contact info"
     erb :contact
+end
+
+post "/response" do
+
+  from = Email.new(email: params[:from])
+  subject = params[:subject]
+  to = Email.new(email: 'matt.fasano88@gmail.com')
+  content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+
+  mail = Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  puts response.status_code
+  puts response.body
+  puts response.headers
+
+  redirect "/contact"
+
 end
